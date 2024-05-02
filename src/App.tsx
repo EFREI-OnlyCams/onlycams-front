2a4fcfa9efd5fe95ede1a112c11ecfc6fa1ac647
Ulrich ZHAN <ulrich.zhan@live.fr>
2024-05-02 15:51:32 +0200

    base

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Link, Routes, Navigate} from 'react-router-dom';
+import PageNotFound from './pages/page-not-found';
+import Login from './pages/login';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+      <nav> 
+        <div className="nav-wrapper teal">
+          <Link to="/" className="brand-logo Left">OnylyCams</Link>
+        </div> 
+      </nav>
+      <Routes>
+		<Route path="/login" element={<Login/>} />
+		<Route path="*" element={<PageNotFound/>} />
+
+		<Route path="/" element={<Navigate to="/login" />} />
+      </Routes>
+      </div>
+    </Router>
   );
 }
 
